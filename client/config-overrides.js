32b5eb8782a26dff821196ed5efcefd83fb55926
seldii <seldaguzel@gmail.com>
2019-09-09 14:52:32 +0200

    deploy try

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -2,11 +2,16 @@ const { override } = require("customize-cra");
 const cspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
 
 const cspConfigPolicy = {
-  "default-src": "'none'",
-  "base-uri": "'self'",
-  "object-src": "'none'",
-  "script-src": ["'self'"],
-  "style-src": ["'self'"]
+  defaultSrc: ["'self'", "default.com"],
+  scriptSrc: ["'self'", "'unsafe-inline'"],
+  styleSrc: ["style.com"],
+  fontSrc: ["'self'", "fonts.com"],
+  imgSrc: ["img.com", "data:"],
+  sandbox: ["allow-forms", "allow-scripts"],
+  reportUri: "/report-violation",
+  objectSrc: ["'none'"],
+  upgradeInsecureRequests: true,
+  workerSrc: false // This is not set.
 };
 
 function addCspHtmlWebpackPlugin(config) {
